38f30eea1a4b9bfb4abc7ec617d3b6ebfd4f87d4
Timofei Zhakov <tima@chemodax.net>
2026-08-06 21:57:49 +0000

    move creation of new series buttons onto the navbar
    git-svn-id: file:///home/repos/rinrab/sailresults/trunk@6912 0083e6b4-6090-234a-836e-88fcc467669c

--- src/home.tsx
+++ src/home.tsx
@@ -1,5 +1,5 @@
-import { Button, Divider, Text, Card, Body1, CardFooter, MenuTrigger, Menu, MenuPopover, MenuItem, CardHeader } from "@fluentui/react-components";
-import { MoreHorizontalRegular, Open16Regular } from "@fluentui/react-icons";
+import { Button, Divider, Text, Card, Body1, CardFooter, MenuTrigger, Menu, MenuPopover, MenuItem, CardHeader, SplitButton, MenuGroupHeader, MenuDivider } from "@fluentui/react-components";
+import { BookQuestionMarkRegular, DocumentAddRegular, MoreHorizontalRegular, NewRegular, Open16Regular } from "@fluentui/react-icons";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Content, Layout, NavBar, NavBarItem } from "./common";
@@ -63,32 +63,35 @@ export default function StartState() {
     <Layout>
       <NavBar>
         <NavBarItem title="Main Menu" to="" />
+        <div style={{ flex: 1 }} />
+        <Menu positioning="below-end">
+          <MenuTrigger>
+            {(triggerProps) => (
+              <SplitButton
+                menuButton={triggerProps}
+                icon={<DocumentAddRegular /> }
+                primaryActionButton={{ onClick: () => navigate("series/new") }} />
+            )}
+          </MenuTrigger>
+
+          <MenuPopover>
+            <MenuItem onClick={() => navigate("series/new")}>Blank new Series</MenuItem>
+            <MenuDivider />
+            <MenuGroupHeader>Sample Regattas</MenuGroupHeader>
+            {samples.map((sample, index) =>
+              <MenuItem onClick={() => createSample(sample)}
+                        key={index}>{sample.name}</MenuItem>
+            )}
+          </MenuPopover>
+        </Menu>
+        <Button onClick={() => navigate("docs")}
+                icon={<BookQuestionMarkRegular />}
+                style={{ marginLeft: 4 }} />
       </NavBar>
       <Content>
         <div style={{ overflow: "auto" }}>
           <div style={{ textAlign: "center" }}>
             <h1>SailResults</h1>
-            <div>
-              <Button style={{ width: 200, marginRight: 8 }} 
-                      onClick={() => navigate("docs")}>
-                Read the docs
-              </Button>
-              <Button style={{ width: 200, marginRight: 8 }} 
-                      onClick={() => navigate(`/series/new`)}>
-                Create New Series
-              </Button>
-              <Menu>
-                <MenuTrigger>
-                  <Button style={{ width: 200 }}>Sample regattas</Button>
-                </MenuTrigger>
-                <MenuPopover>
-                  {samples.map((sample, index) =>
-                    <MenuItem onClick={() => createSample(sample)}
-                              key={index}>{sample.name}</MenuItem>
-                  )}
-                </MenuPopover>
-              </Menu>
-            </div>
           </div>
           <Divider style={{ margin: "8px 0" }} />
           <h2>Recent Series</h2>
