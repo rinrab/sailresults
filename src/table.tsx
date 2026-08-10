a24a6fef77557874c001898d4f1f5550def432da
Timofei Zhakov <tima@chemodax.net>
2026-08-10 14:22:12 +0000

    use virtualized table for the results
    git-svn-id: file:///home/repos/rinrab/sailresults/trunk@6964 0083e6b4-6090-234a-836e-88fcc467669c

--- src/table.tsx
+++ src/table.tsx
@@ -3,10 +3,11 @@ import { useVirtualizer } from "@tanstack/react-virtual";
 import React from "react";
 
 export interface Column<T> {
-  header: string;
+  header: string | React.ReactElement;
   cell: (row: T) => React.ReactElement;
   size?: number;
   minsize?: number;
+  align?: "start" | "center" | "end";
 }
 
 export interface SailTableProps<KeyT, ValueT> {
@@ -27,18 +28,20 @@ export function SailTable<KeyT, ValueT>(props: SailTableProps<KeyT, ValueT>) {
 
   const getCellStyles = (col: Column<ValueT>) => {
     const style: React.CSSProperties = {
-       display: "flex",
-       alignItems: "center",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: col.align,
+      textAlign: col.align,
     }
 
     if (col.size) {
+      style.flex = "0 0 auto";
       style.width = col.size;
+    } else if (col.minsize) {
+      style.flex = 1;
+      style.minWidth = col.minsize;
     } else {
       style.flex = 1;
-    }
-
-    if (col.minsize) {
-      style.minWidth = col.minsize;
     }
 
     return style;
@@ -83,7 +86,6 @@ export function SailTable<KeyT, ValueT>(props: SailTableProps<KeyT, ValueT>) {
                           display: "flex",
                           height: item.size,
                           position: "absolute",
-                          width: "100%",
                           transform: `translateY(${item.start}px)`,
                           cursor: props.onSelect ? "pointer" : "default",
                         }}
