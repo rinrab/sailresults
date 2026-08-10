b66932c66f71f2bd73ccac83679d7c62ae50f8b0
Timofei Zhakov <tima@chemodax.net>
2026-08-10 15:20:40 +0000

    revert merge
    git-svn-id: file:///home/repos/rinrab/sailresults/branches/deploy-version3@6982 0083e6b4-6090-234a-836e-88fcc467669c

--- src/docs.tsx
+++ src/docs.tsx
@@ -3,7 +3,6 @@ import { Content, Layout, NavBar, NavBarItem } from "./common";
 import React from "react";
 import { Link, Text } from "@fluentui/react-components";
 import { dsqs } from "./scoring";
-import { version_major, version_minor, revnum } from "./version";
 
 export function FeaturesList() {
   return <ul>
@@ -56,21 +55,16 @@ export function DocsAbout() {
           <h3>Features</h3>
           <FeaturesList />
 
-        <h3>Credits</h3>
-        <ul>
-          <li>Timofei Zhakov - I'm the author, hey guys!</li>
-          <li>Rautu - The idea and early adoption.</li>
-          <li>SailWave a great alternative that does a similar job. If you
-          look for long enough, you'd find any exotic feature you want.
-          It's also free btw.</li>
-        </ul>
-        <h3>Version</h3>
-        <ul>
-          <li>Version: {version_major}.{version_minor} {revnum == "staging" && ("(in development)")}</li>
-          <li>Subversion revision number: {revnum}</li>
-        </ul>
-      </div>
-    </Content>
+          <h3>Credits</h3>
+          <ul>
+            <li>Timofei Zhakov - I'm the author, hey guys!</li>
+            <li>Rautu - The idea and early adoption.</li>
+            <li>SailWave a great alternative that does a similar job. If you
+            look for long enough, you'd find any exotic feature you want.
+            It's also free btw.</li>
+          </ul>
+        </div>
+      </Content>
   </Layout>
 }
 
--- src/version.ts
+++
@@ -1,3 +0,0 @@
-export const version_major = 1;
-export const version_minor = 0;
-export const revnum = "staging";
