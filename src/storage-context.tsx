26a4b783989287f7c8655bd9b127adbab606ea9f
Timofei Zhakov <tima@chemodax.net>
2026-08-17 22:13:12 +0000

    add firebase and upload series there
    git-svn-id: file:///home/repos/rinrab/sailresults/branches/firebase@7084 0083e6b4-6090-234a-836e-88fcc467669c

--- src/storage-context.tsx
+++ src/storage-context.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 import { getStorageEditor, IStorage, openLegacyRacers, openSeries, saveSeries } from "./storage";
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { Series } from "./scoring";
 
 export const StorageContext = React.createContext<IStorage>(null);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAy8l4WJmdHngHhoY-z7-L1VL1WG8vB_Yc",
+  authDomain: "sailresults-staging.firebaseapp.com",
+  projectId: "sailresults-staging",
+  storageBucket: "sailresults-staging.firebasestorage.app",
+  messagingSenderId: "502927475203",
+  appId: "1:502927475203:web:d43f5059493cb2ab3e4436"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const seriesStore = collection(db, "series");
+
+async function SyncSeries(series: Series) {
+  try {
+    const docRef = await addDoc(seriesStore, series);
+
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
 
 export function StorageProvider({ children }) {
   let legacyRacers = React.useMemo(() => openLegacyRacers(), []);
@@ -12,6 +39,9 @@ export function StorageProvider({ children }) {
      (mutate) => setSeries((old) => {
        series = mutate(old);
        saveSeries(series);
+
+       Object.values(editor.listSeries()).map(series => SyncSeries(series));
+
        return series;
      }),
   );
