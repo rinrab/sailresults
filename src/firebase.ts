21c7debd6285baa4169cad38dda4d9a32980482e
Timofei Zhakov <tima@chemodax.net>
2026-08-18 12:11:47 +0000

    refactoring: factor out firebase stuff
    git-svn-id: file:///home/repos/rinrab/sailresults/branches/firebase@7088 0083e6b4-6090-234a-836e-88fcc467669c

---
+++ src/firebase.ts
@@ -0,0 +1,88 @@
+import { IStorage } from "./storage";
+import { initializeApp } from "firebase/app";
+import { doc, addDoc, collection, getFirestore, setDoc, getDocs, where, query } from "firebase/firestore";
+import { Series } from "./scoring";
+import { getAuth } from "firebase/auth";
+import { storage } from "./storage-context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAy8l4WJmdHngHhoY-z7-L1VL1WG8vB_Yc",
+  authDomain: "sailresults-staging.firebaseapp.com",
+  projectId: "sailresults-staging",
+  storageBucket: "sailresults-staging.firebasestorage.app",
+  messagingSenderId: "502927475203",
+  appId: "1:502927475203:web:d43f5059493cb2ab3e4436"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const db = getFirestore(app);
+const seriesStore = collection(db, "series");
+
+let lastPulledId = null;
+
+async function pull(editor: IStorage) {
+  if (auth.currentUser) {
+    if (lastPulledId == auth.currentUser.uid) {
+      return;
+    }
+
+    const snapshot = await getDocs(
+      query(seriesStore, where("owner", "==", auth.currentUser.uid))
+    )
+
+    console.log(snapshot.docs);
+  } else {
+    console.log("can't pull: unauthorized");
+  }
+}
+
+async function push(editor: IStorage) {
+  if (auth.currentUser) {
+    const promises = Object.values(editor.listSeries())
+      .map(series => SyncSeries(editor, series));
+
+    await Promise.all(promises);
+  } else {
+    console.log("can't push: unauthorized");
+  }
+}
+
+async function SyncSeries(storage: IStorage, series: Series) {
+  const resource = {
+    ...series,
+    owner: auth.currentUser.uid,
+  };
+
+  try {
+    if (series.firebaseId) {
+      await setDoc(doc(seriesStore, series.firebaseId), resource);
+    } else {
+      const docRef = await addDoc(seriesStore, resource);
+      const editor = storage.openSeries(series.id);
+      editor.setFirebaseId(docRef.id);
+    }
+
+    console.log("synced", resource);
+  } catch (e) {
+    console.error("Error syncing document: ", e, resource);
+  }
+}
+
+let pushTimeout: NodeJS.Timeout | null = null;
+
+export function initializeFirebase() {
+  auth.onAuthStateChanged(() => schedulePush());
+  pull(storage);
+}
+
+export function schedulePush() {
+  if (pushTimeout) {
+    clearTimeout(pushTimeout);
+  }
+
+  pushTimeout = setTimeout(() => {
+    push(storage);
+  }, 1000);
+}
--- src/storage-context.tsx
+++ src/storage-context.tsx
@@ -1,86 +1,17 @@
 import React from "react";
 import { getStorageEditor, IStorage, openLegacyRacers, openSeries, saveSeries } from "./storage";
-import { initializeApp } from "firebase/app";
-import { doc, addDoc, collection, getFirestore, runTransaction, setDoc, getDocs, where, query } from "firebase/firestore";
-import { Series } from "./scoring";
-import { getAuth } from "firebase/auth";
+import { initializeFirebase, schedulePush } from "./firebase";
 
-export const StorageContext = React.createContext<IStorage>(null);
+export const StorageContext = React.createContext(null);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAy8l4WJmdHngHhoY-z7-L1VL1WG8vB_Yc",
-  authDomain: "sailresults-staging.firebaseapp.com",
-  projectId: "sailresults-staging",
-  storageBucket: "sailresults-staging.firebasestorage.app",
-  messagingSenderId: "502927475203",
-  appId: "1:502927475203:web:d43f5059493cb2ab3e4436"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-
-const db = getFirestore(app);
-const seriesStore = collection(db, "series");
-
-let lastPulledId = null;
-
-async function pull(editor: IStorage) {
-  if (auth.currentUser) {
-    if (lastPulledId == auth.currentUser.uid) {
-      return;
-    }
-
-    const snapshot = await getDocs(
-      query(seriesStore, where("owner", "==", auth.currentUser.uid))
-    )
-
-    console.log(snapshot.docs);
-  } else {
-    console.log("can't pull: unauthorized");
-  }
-}
-
-async function push(editor: IStorage) {
-  if (auth.currentUser) {
-    const promises = Object.values(editor.listSeries())
-      .map(series => SyncSeries(editor, series));
-
-    await Promise.all(promises);
-  } else {
-    console.log("can't push: unauthorized");
-  }
-}
-
-async function SyncSeries(storage: IStorage, series: Series) {
-  const resource = {
-    ...series,
-    owner: auth.currentUser.uid,
-  };
-
-  try {
-    if (series.firebaseId) {
-      await setDoc(doc(seriesStore, series.firebaseId), resource);
-    } else {
-      const docRef = await addDoc(seriesStore, resource);
-      const editor = storage.openSeries(series.id);
-      editor.setFirebaseId(docRef.id);
-    }
-
-    console.log("synced", resource);
-  } catch (e) {
-    console.error("Error syncing document: ", e, resource);
-  }
-}
-
-
-let pushTimeout: NodeJS.Timeout | null = null;
 let initialized = false;
+export let storage: IStorage;
 
 export function StorageProvider({ children }) {
   let legacyRacers = React.useMemo(() => openLegacyRacers(), []);
   let [series, setSeries] = React.useState(() => openSeries(legacyRacers));
 
-  const storage = getStorageEditor(
+  storage = getStorageEditor(
      () => series, 
      (mutate) => setSeries((old) => {
        series = mutate(old);
@@ -89,23 +20,12 @@ export function StorageProvider({ children }) {
      }),
   );
 
-  const schedule = () => {
-    if (pushTimeout) {
-      clearTimeout(pushTimeout);
-    }
-
-    pushTimeout = setTimeout(() => {
-      push(storage);
-    }, 1000);
-  };
-
   if (! initialized) {
     initialized = true;
-    auth.onAuthStateChanged(() => schedule());
-    pull(storage);
+    initializeFirebase();
   }
 
-  storage.onSeriesChanged(() => schedule());
+  storage.onSeriesChanged(() => schedulePush());
 
   return (
     <StorageContext.Provider value={storage}>
